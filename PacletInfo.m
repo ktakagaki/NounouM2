fd0d9162d2db94a86853963a610df43f39d66cd9
ktakagaki <kentaroh.takagaki@gmail.com>
2013-11-06 18:37:55 +0100

    Initial

---
+++ PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2013.11.06*)
+
+Paclet[
+  Name -> "NounouM2",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
