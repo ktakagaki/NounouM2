7d4149e06193701fbdc16db0eec967aa769084ea
ktakagaki <kentaroh.takagaki@gmail.com>
2014-02-13 11:55:11 +0100

    Started documentation, started structure of plotting

---
+++ DataPlotting.m
@@ -0,0 +1,10 @@
+(* Mathematica Package *)
+
+BeginPackage["NounouM2`DataPlotting`", { "JLink`", "NounouM2`", "NounouM2`Plotting`"}]
+(* Exported symbols added here with SymbolName::usage *)  
+
+Begin["`Private`"] (* Begin Private Context *) 
+
+End[] (* End Private Context *)
+
+EndPackage[]
---
+++ Plotting.m
@@ -0,0 +1,10 @@
+(* Mathematica Package *)
+
+BeginPackage["NounouM2`Plotting`", { "JLink`", "NounouM2`"}]
+(* Exported symbols added here with SymbolName::usage *)  
+
+Begin["`Private`"] (* Begin Private Context *) 
+
+End[] (* End Private Context *)
+
+EndPackage[]
